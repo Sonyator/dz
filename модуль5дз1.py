476196831a709995cfe5ae44bb77e32c6dbdb303
Sonyator <132260364+Sonyator@users.noreply.github.com>
2023-05-04 13:42:31 +0300

    Add files via upload

---
+++ модуль5дз1.py
@@ -0,0 +1,11 @@
+class StringVar:
+      def __init__(self):
+            self.s=''
+      def set(self,val):
+            self.s=val
+      def get(self):
+          return self.s
+s=StringVar()
+print(s.set('привет'))
+print(s.get())
+
